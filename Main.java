6db9d342ba486a3400207d67645a8f078247da12
langlibaihu <2754475247@qq.com>
2022-04-19 23:54:38 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("asdasd");
+        System.out.println("asdsa奥术大师多撒大所");
+        System.out.println("sadasdh暗示大家好asddsadj爱神的箭雷克萨建档立卡是");
+        System.out.println("阿斯顿撒阿斯顿撒asdsadas");
+        System.out.println("sad");
+    }
+}
